de76a29c8f35b25fe742e9eb45601d1a8e275131
muturgan <34779116+muturgan@users.noreply.github.com>
2018-01-16 02:58:07 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+﻿'use strict';
+
+/**
+ * Реализовать RLE-сжатие: AAAB -> A3B, BCCDDDAXXXX -> BC2D3AX4
+ * @param  {string} value
+ * @return {string}
+ 
+ автор - Сахаров Андрей
+ */
+function rle(value) {
+    let repetition = 0;
+    let result = '';
+    
+    for (let i = 0; i < value.length; i++) {
+        
+        if (value[i] === value[i + 1]) {
+            ++repetition;
+        } else {
+            result = result + value[i];
+            if (repetition > 0) {
+                repetition++;
+                result = result + repetition;
+            }
+            repetition = 0;
+        }
+    }
+    
+    return result;
+}
+
+
+console.log(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'));
